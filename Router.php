46f82ba6893b6da34817cc878db6bb00cfd9439d
Renzo <rjimenezg05@gmail.com>
2026-09-09 20:36:59 -0700

    Tercer commit

--- Router.php
+++ Router.php
@@ -21,7 +21,9 @@ class Router
     {
         
         // Proteger Rutas...
-        session_start();
+        if(!isset($_SESSION)) {
+            session_start();
+        }; 
 
         // Arreglo de rutas protegidas...
         // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
